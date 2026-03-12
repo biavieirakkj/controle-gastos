9c12421fd400c20ad9696ce92292b1001431a301
beatriz.silva <beatriz.silva@tre-ms.jus.br>
2026-03-12 16:28:59 -0400

    adição de transaçoes na página home

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,3 +6,40 @@ function logout()
         alert('Erro ao fazer logout')
     })
 }
+
+const fakeTransactions = [{
+    type: 'expense',
+    date: '2026-01-05',
+    money: {
+        currency: 'R$',
+        value: 20
+    },
+    transactionType: 'Alimentação',
+    description: 'Atacadão'
+}, {
+    type: 'income',
+    date: '2026-01-04',
+    money: {
+        currency: 'R$',
+        value: 1612
+    },
+    transactionType: 'Bolsa estudo'
+}, {
+    type: 'expense',
+    date: '2026-01-01',
+    money: {
+        currency: 'EUR',
+        value: 100
+    },
+    transactionType: 'Lazer',
+    description: 'Maquiagem marca tal'
+}, {
+    type: 'expense',
+    date: '2026-01-01',
+    money: {
+        currency: 'USD',
+        value: 37
+    },
+    transactionType: 'Trabalho',
+    description: 'Melhoria homeoffice'
+}]
