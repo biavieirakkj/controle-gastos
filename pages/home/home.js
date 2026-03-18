76ad4a093b5db691734e712ee90feb6577266a31
beatriz.silva <beatriz.silva@tre-ms.jus.br>
2026-03-18 15:15:30 -0400

    funcoes da pagina inicial

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,6 +7,59 @@ function logout()
     })
 }
 
+findTransactions();
+
+function findTransactions()
+{
+    setTimeout(() => 
+    {
+        addTransactionToScreen(fakeTransactions)
+    }, 1000)
+}
+
+function addTransactionToScreen(transactions)
+{
+    const orderedList = document.getElementById('transactions');
+
+    transactions.forEach(transaction => { 
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(transaction.date);
+        li.appendChild(date);
+
+        const money = document.createElement('p');
+        money.innerHTML = formatMoney(transaction.money);
+        li.appendChild(money);
+
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+
+        if(transaction.description)
+        {
+            const description = document.createElement('p');
+            description.innerHTML = transaction.description;
+            li.appendChild(description);
+        }
+
+        orderedList.appendChild(li);
+
+
+    });
+}
+
+function formatMoney(money)
+{
+    return `${money.currency} ${money.value.toFixed(2)}`
+}
+
+function formatDate(date)
+{
+    return new Date(date).toLocaleDateString('pt-br');
+}
+
 const fakeTransactions = [{
     type: 'expense',
     date: '2026-01-05',
