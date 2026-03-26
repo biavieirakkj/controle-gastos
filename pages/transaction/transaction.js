7ed070dff78f40cc6f3dd7fcc1830844252010ce
beatriz.silva <beatriz.silva@tre-ms.jus.br>
2026-03-26 17:49:20 -0400

    parte 2 da página de transacoes adiciao de funçoes para adicionar funcoes no banco e adicao de funçoes de mensagens de erro

--- pages/transaction/transaction.js
+++ pages/transaction/transaction.js
@@ -5,6 +5,51 @@ function logout()
     }).catch(() => {
         alert('Erro ao fazer logout')
     })
+}
+
+function saveTransaction()
+{
+    showLoading();
+
+    const transaction = createTransaction();
+
+    firebase.firestore()
+        .collection('transactions')
+        .add(transaction)
+        .then(() => {
+            hideLoading();
+            window.location.href = "../home/home.html";
+        })
+        .catch(() => {
+            hideLoading();
+            alert('Erro ao salvar transação');
+        })
+}
+
+function createTransaction()
+{
+    return {
+        type: form.typeExpense().checked ? "expense" : "income",
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    }
+}
+
+function logout()
+{
+    firebase.auth().signOut().then(() => {
+        window.location.href = "../../index.html"
+    }).catch(() => {
+        alert('Erro ao fazer logout')
+    })
 
 }
 
@@ -12,6 +57,8 @@ function onChangeDate()
 {
     const date = form.date().value;
     form.dateRequiredError().style.display = !date ? "block" : "none";
+
+    toggleSaveButtonDisable();
 }
 
 function onChangeValue()
@@ -20,14 +67,57 @@ function onChangeValue()
     form.valueRequiredError().style.display = !value ? "block" : "none";
 
     form.valueLessOrEqualToZeroError().style.display = value <= 0 ? "block" : "none";
+
+    toggleSaveButtonDisable();
+}
+
+function onChangeTransactionType()
+{
+    const transactionType = form.transactionType().value;
+    form.transactionTypeRequiredError().style.display = !transactionType ? "block" : "none";
+
+    toggleSaveButtonDisable();
+}
+
+function toggleSaveButtonDisable()
+{
+    form.saveButton().disabled = !isFormValid();
+}
+
+function isFormValid()
+{
+    const date = form.date().value;
+    if(!date)
+    {
+        return false;
+    }
+
+    const value = form.value().value;
+    if(!value || value <= 0)
+    {
+        return false;
+    }
+
+    const transactionType = form.transactionType().value;
+    if(!transactionType)
+    {
+        return false;
+    }
+
+    return true;
 }
 
 const form = 
 {
+    typeExpense: () => document.getElementById('expense'),
     date: () => document.getElementById('date'),
     dateRequiredError: () => document.getElementById('date-required-error'),
-    value: () => document.getElementById('date'),
+    currency: () => document.getElementById('currency'),
+    transactionType: () => document.getElementById('transaction-type'),
+    transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
+    value: () => document.getElementById('value'),
     valueRequiredError: () => document.getElementById('value-required-error'),
-    valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error')
-
+    valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error'),
+    description: () => document.getElementById('description'),
+    saveButton: () => document.getElementById('save-button')
 }
